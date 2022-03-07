1e17df5e8d691a0052dcf92b585fe8934072e376
CharlyCervera <ccervera.cc@hotmail.com>
2022-03-07 08:40:03 -0600

    Unit2 Finally Released

---
+++ Unit2/loops.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+    
+    printf("1\n");
+    printf("2\n");
+    printf("3\n");
+    printf("4\n");
+
+    for (int numero = 1; numero <= 5; numero = numero + 1){
+        printf("mi numero es : %d\n", numero);
+    }
+printf("Last count: %d\n", numero);
+
+return 0;
+
+}
